4f5ea53c2ca7e254bff59251747e3e77977adb46
mohamed-elidrissi <mohamedel62840@gmail.com>
2026-07-03 10:38:17 +0100

    Ajout de l'incrémentation et de la réinitialisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    </>
-  )
+    <div className="container">
+      <div className="count">{count}</div>
+
+      <div className="buttons">
+        <button onClick={() => setCount((count) => count + 1)}>
+          +1
+        </button>
+
+        <button onClick={() => setCount((count) => (count > 0 ? count - 1 : 0))}>
+          -1
+        </button>
+
+        <button onClick={() => setCount(0)}>
+          Reinitialize
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
