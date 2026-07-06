dc7c839dc71854bae9c907032e3655e63a7b1eb4
mohamed-elidrissi <mohamedel62840@gmail.com>
2026-07-06 10:33:17 +0100

    changement nom de class

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="container">
-      <div className="count">{count}</div>
+      <div className="count1">{count}</div>
 
       <div className="buttons">
         <button onClick={() => setCount((count) => count + 1)}>
