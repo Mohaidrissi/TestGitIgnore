46b2a70efa38ddfea0839f8501a16a02254f4df1
MouradAfpa <mourad.arab.formateur.afpa@gmail.com>
2026-07-06 11:51:42 +0200

    modification App.jsx, App.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="container">
-      <div className="count">{count}</div>
+      <div className="count2">{count}</div>
 
       <div className="buttons">
         <button onClick={() => setCount((count) => count + 1)}>
